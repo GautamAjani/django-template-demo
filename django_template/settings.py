602fcfa280894eab44d3e28c58cd02f57ac01a5f
Gautam <gautamatrt@gmail.com>
2019-09-17 19:18:55 +0530

    add the user form code

--- django_template/settings.py
+++ django_template/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,6 +41,7 @@ INSTALLED_APPS = [
 
     # app
     'blog',
+    'user',
 ]
 
 MIDDLEWARE = [
